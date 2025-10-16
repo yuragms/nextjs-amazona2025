69be951c265fb8a6e847abaf2ca75efd51548928
yuragms <yura.gms@gmail.com>
2025-10-16 18:31:19 +0300

    13-create-cart-sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { APP_DESCRIPTION, APP_NAME, APP_SLOGAN } from '@/lib/constants'
+import ClientProviders from '@/components/shared/client-providers'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -31,7 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <ClientProviders>{children}</ClientProviders>
       </body>
     </html>
   )
--- components/shared/header/cart-button.tsx
+++ components/shared/header/cart-button.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import useIsMounted from '@/hooks/use-is-mounted'
 import { cn } from '@/lib/utils'
 import useCartStore from '@/hooks/use-cart-store'
+import useCartSidebar from '@/hooks/use-cart-sidebar'
 
 export default function CartButton() {
   const isMounted = useIsMounted()
@@ -12,6 +13,7 @@ export default function CartButton() {
     cart: { items },
   } = useCartStore()
   const cartItemsCount = items.reduce((a, c) => a + c.quantity, 0)
+  const isCartSidebarOpen = useCartSidebar()
   return (
     <Link href='/cart' className='px-1 header-button'>
       <div className='flex items-end text-xs relative'>
@@ -28,6 +30,11 @@ export default function CartButton() {
           </span>
         )}
         <span className='font-bold'>Cart</span>
+        {isCartSidebarOpen && (
+          <div
+            className={`absolute top-[20px] right-[-16px] rotate-[-90deg] z-10 w-0 h-0 border-l-[7px] border-r-[7px] border-b-[8px] border-transparent border-b-background`}
+          ></div>
+        )}
       </div>
     </Link>
   )
