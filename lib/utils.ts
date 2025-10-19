1cb269b75d5de9d5a5c76967f005ee8ac6e8302a
yuragms <yura.gms@gmail.com>
2025-10-19 12:23:53 +0300

    15-register-user

--- lib/utils.ts
+++ lib/utils.ts
@@ -40,3 +40,28 @@ export const round2 = (num: number) =>
 
 export const generateId = () =>
   Array.from({ length: 24 }, () => Math.floor(Math.random() * 10)).join('')
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const formatError = (error: any): string => {
+  if (error.name === 'ZodError') {
+    const fieldErrors = Object.keys(error.errors).map((field) => {
+      const errorMessage = error.errors[field].message
+      return `${error.errors[field].path}: ${errorMessage}` // field: errorMessage
+    })
+    return fieldErrors.join('. ')
+  } else if (error.name === 'ValidationError') {
+    const fieldErrors = Object.keys(error.errors).map((field) => {
+      const errorMessage = error.errors[field].message
+      return errorMessage
+    })
+    return fieldErrors.join('. ')
+  } else if (error.code === 11000) {
+    const duplicateField = Object.keys(error.keyValue)[0]
+    return `${duplicateField} already exists`
+  } else {
+    // return 'Something went wrong. please try again'
+    return typeof error.message === 'string'
+      ? error.message
+      : JSON.stringify(error.message)
+  }
+}
--- types/index.ts
+++ types/index.ts
@@ -4,6 +4,7 @@ import {
   ProductInputSchema,
   UserInputSchema,
   UserSignInSchema,
+  UserSignUpSchema,
 } from '@/lib/validator'
 import { z } from 'zod'
 
@@ -31,3 +32,4 @@ export type Cart = z.infer<typeof CartSchema>
 // user
 export type IUserInput = z.infer<typeof UserInputSchema>
 export type IUserSignIn = z.infer<typeof UserSignInSchema>
+export type IUserSignUp = z.infer<typeof UserSignUpSchema>
