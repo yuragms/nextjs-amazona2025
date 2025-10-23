17c629915ceac60906a71fc72ed1a8f2bd6988a8
yuragms <yura.gms@gmail.com>
2025-10-23 13:05:02 +0300

    20-pay-order-by-stripe

--- app/checkout/[id]/page.tsx
+++ app/checkout/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from 'next/navigation'
 import React from 'react'
-
+import Stripe from 'stripe'
 import { auth } from '@/auth'
 import { getOrderById } from '@/lib/actions/order.actions'
 import PaymentForm from './payment-form'
@@ -15,19 +15,29 @@ const CheckoutPaymentPage = async (props: {
   }>
 }) => {
   const params = await props.params
-
   const { id } = params
-
   const order = await getOrderById(id)
   if (!order) notFound()
 
   const session = await auth()
+
+  let client_secret = null
+  if (order.paymentMethod === 'Stripe' && !order.isPaid) {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: Math.round(order.totalPrice * 100),
+      currency: 'USD',
+      metadata: { orderId: order._id },
+    })
+    client_secret = paymentIntent.client_secret
+  }
 
   return (
     <PaymentForm
       order={order}
       paypalClientId={process.env.PAYPAL_CLIENT_ID || 'sb'}
       isAdmin={session?.user?.role === 'Admin' || false}
+      clientSecret={client_secret}
     />
   )
 }
--- app/checkout/[id]/payment-form.tsx
+++ app/checkout/[id]/payment-form.tsx
@@ -18,14 +18,19 @@ import CheckoutFooter from '../checkout-footer'
 import { redirect, useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
 import ProductPrice from '@/components/shared/product/product-price'
+import { loadStripe } from '@stripe/stripe-js'
+import { Elements } from '@stripe/react-stripe-js'
+import StripeForm from './stripe-form'
 
 export default function OrderPaymentForm({
   order,
   paypalClientId,
+  clientSecret,
 }: {
   order: IOrder
   paypalClientId: string
   isAdmin: boolean
+  clientSecret: string | null
 }) {
   const router = useRouter()
   const {
@@ -125,6 +130,19 @@ export default function OrderPaymentForm({
                 </PayPalScriptProvider>
               </div>
             )}
+            {!isPaid && paymentMethod === 'Stripe' && clientSecret && (
+              <Elements
+                options={{
+                  clientSecret,
+                }}
+                stripe={stripePromise}
+              >
+                <StripeForm
+                  priceInCents={Math.round(order.totalPrice * 100)}
+                  orderId={order._id}
+                />
+              </Elements>
+            )}
 
             {!isPaid && paymentMethod === 'Cash On Delivery' && (
               <Button
@@ -138,6 +156,9 @@ export default function OrderPaymentForm({
         </div>
       </CardContent>
     </Card>
+  )
+  const stripePromise = loadStripe(
+    process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
   )
 
   return (
